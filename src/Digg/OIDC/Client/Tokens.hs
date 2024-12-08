aebdfa93ef5610181db5c54b40fef3ffd7daabda
Tomas Stenlund <tomas.stenlund@telia.com>
2024-12-08 09:21:31 +0100

    Removed unused symbols and corrected faulty comments after type rework of the token

--- src/Digg/OIDC/Client/Tokens.hs
+++ src/Digg/OIDC/Client/Tokens.hs
@@ -11,8 +11,7 @@
 --    The module defines types and functions for handling OIDC tokens and validations of standard claims as well
 --    as custom claims.
 module Digg.OIDC.Client.Tokens
-  ( Tokens (..),
-    Claims (..),
+  ( Claims (..),
     IdClaims (..),
     AccessClaims (..),
     NoExtraClaims (..),
@@ -26,6 +25,8 @@ where
 
 import           Control.Applicative                 (optional, (<|>))
 import           Control.Exception                   (throwIO)
+import           Control.Monad                       (unless)
+import           Control.Monad.Catch                 (MonadThrow (throwM))
 import           Control.Monad.Except                (catchError)
 import           Control.Monad.IO.Class              (MonadIO, liftIO)
 import           Data.Aeson                          (FromJSON (parseJSON),
@@ -37,31 +38,29 @@ import qualified Data.ByteString.Lazy.Char8          as BL
 import           Data.Either                         (partitionEithers)
 import           Data.Text                           (Text, pack)
 import           Data.Text.Encoding                  (encodeUtf8)
+import           Data.Time.Clock.POSIX               (getPOSIXTime)
 import           Digg.OIDC.Client                    (OIDC (..),
                                                       OIDCException (..))
 import           Digg.OIDC.Client.Discovery.Provider (Algorithm (..),
                                                       Provider (..),
                                                       ProviderMetadata (..))
+import           Digg.OIDC.Types                     (Nonce)
 import           GHC.Generics                        (Generic)
 import           Jose.Jwt                            (IntDate (..), Jwt,
                                                       JwtContent (Jwe, Jws, Unsecured))
 import qualified Jose.Jwt                            as Jwt
 import           Prelude                             hiding (exp)
-import Digg.OIDC.Types (Nonce)
-import Control.Monad (unless)
-import Control.Monad.Catch (MonadThrow(throwM))
-import Data.Time.Clock.POSIX (getPOSIXTime)
 
--- | The 'TokenClaims' data type represents the claims contained within a token.
+-- | The 'Claims' data type represents the claims contained within a token and specific for any JWT.
 -- The type parameter 'a' allows for flexibility in specifying additional claims.
 -- This can be used to define different kinds of tokens with varying claim structures.
 data Claims a = Claims
-  { iss         :: !Text,     -- ^ The issuer of the token
-    sub         :: !Text,     -- ^ The subject of the token
-    aud         :: ![Text],   -- ^ The audience of the token
-    exp         :: !IntDate,  -- ^ The expiration time of the token
-    iat         :: !IntDate,  -- ^ The time the token was issued
-    jti         :: !Text,     -- ^ The JWT ID of the token
+  { iss          :: !Text,     -- ^ The issuer of the token
+    sub          :: !Text,     -- ^ The subject of the token
+    aud          :: ![Text],   -- ^ The audience of the token
+    exp          :: !IntDate,  -- ^ The expiration time of the token
+    iat          :: !IntDate,  -- ^ The time the token was issued
+    jti          :: !Text,     -- ^ The JWT ID of the token
 
     other_claims :: !a     -- ^ Additional claims of the token, defined by the user of this library
   }
@@ -78,17 +77,15 @@ instance (FromJSON a) => FromJSON (Claims a) where
       <*> o .: "jti"
       <*> parseJSON (Object o)
 
--- | 'IdTokenClaims' represents the claims contained in an ID token.
+-- | 'IdClaims' represents the claims contained in an ID token.
 -- The type parameter 'a' allows for flexibility in the type of the claims.
--- This data type is typically used in the context of OpenID Connect (OIDC)
--- to hold the information about the authenticated user.
 data IdClaims a = IdClaims
   {
-    id_nonce       :: !(Maybe ByteString),   -- ^ The nonce value of the token
-    id_auth_time   :: !(Maybe IntDate),      -- ^ The time the user authenticated
-    id_acr         :: !(Maybe Text),         -- ^ The authentication context class reference
-    id_amr         :: !(Maybe Text),         -- ^ The authentication methods used
-    id_azp         :: !(Maybe Text),         -- ^ The authorized party
+    id_nonce        :: !(Maybe ByteString),   -- ^ The nonce value of the token
+    id_auth_time    :: !(Maybe IntDate),      -- ^ The time the user authenticated
+    id_acr          :: !(Maybe Text),         -- ^ The authentication context class reference
+    id_amr          :: !(Maybe Text),         -- ^ The authentication methods used
+    id_azp          :: !(Maybe Text),         -- ^ The authorized party
 
     id_other_claims :: !(Maybe a)           -- ^ Additional claims of the id token, defined by the user of this library
 
@@ -103,17 +100,15 @@ instance (FromJSON a) => FromJSON (IdClaims a) where
       <*> o .:? "azp"
       <*> optional (parseJSON (Object o))
 
--- | 'AccessTokenClaims' represents the claims contained in an access token.
+-- | 'AccessClaims' represents the claims contained in an access token.
 -- The type parameter 'a' allows for flexibility in specifying the type of the claims.
--- This data type is typically used in the context of OpenID Connect (OIDC) to
--- handle and validate access tokens issued by an OIDC provider.
 data AccessClaims a = AccessClaims
   {
-    a_scope             :: !(Maybe [Text]), -- ^ The scope of the token
-    a_auth_time   :: !(Maybe IntDate),      -- ^ The time the user authenticated
-    a_acr         :: !(Maybe Text),         -- ^ The authentication context class reference
-    a_amr         :: !(Maybe Text),         -- ^ The authentication methods used
-    a_other_claims :: !(Maybe a)     -- ^ Additional claims of the id token, defined by the user of this library
+    a_scope        :: !(Maybe [Text]), -- ^ The scope of the token
+    a_auth_time    :: !(Maybe IntDate),      -- ^ The time the user authenticated
+    a_acr          :: !(Maybe Text),         -- ^ The authentication context class reference
+    a_amr          :: !(Maybe Text),         -- ^ The authentication methods used
+    a_other_claims :: !(Maybe a)            -- ^ Additional claims of the id token, defined by the user of this library
   } deriving (Show, Eq, Generic)
 
 instance (FromJSON a) => FromJSON (AccessClaims a) where
@@ -126,27 +121,17 @@ instance (FromJSON a) => FromJSON (AccessClaims a) where
       <*> optional (parseJSON (Object o))
 
 -- | 'NoExtraClaims' is a data type representing the absence of additional claims.
---   It is used when no extra claims are needed in the context of OIDC (OpenID Connect) tokens.
---   The only constructor, 'NoClaims', signifies that there are no extra claims.
+--   It is used when no extra claims are needed in the context of the claims.
 data NoExtraClaims = NoClaims
   deriving (Show, Eq, Generic)
 
 instance FromJSON NoExtraClaims
 
+-- | Type aliases for the 'Claims' data type with specific additional claims for the ID token.
 type IdTokenClaims a = Claims (IdClaims a)
-type AccessTokenClaims a = Claims (AccessClaims a)
 
--- | The 'Tokens' data type represents a collection of tokens.
--- The type parameter 'a' allows for flexibility for the identity token stored.
-data Tokens a = Tokens
-  { accessToken  :: Text,           -- ^ The access token
-    tokenType    :: Text,           -- ^ The token type, should always be Bearer
-    idToken      :: Claims a,       -- ^ The identity tokens claims and its additional claims
-    idTokenJwt   :: Jwt,            -- ^ The identity token as a JWT
-    expiresIn    :: Maybe Integer,  -- ^ The expiration time of the token
-    refreshToken :: Maybe Text      -- ^ The refresh token
-  }
-  deriving (Show, Eq)
+-- | Type aliases for the 'Claims' data type with specific additional claims for the access token.
+type AccessTokenClaims a = Claims (AccessClaims a)
 
 -- | Validates a given JWT token using the provided OIDC configuration and returns
 -- the token claims if the token is valid.
@@ -186,7 +171,7 @@ validateToken oidc jwt' = do
       Left err -> liftIO . throwIO . ValidationException $ pack err
 
 -- | Validates the ID token claims.
--- 
+--
 -- This function checks the validity of the ID token claims based on the provided
 -- issuer, audience, and optional nonce. It performs necessary checks to ensure
 -- that the token is valid and has not been tampered with.
@@ -214,7 +199,7 @@ validateIdClaims issuer client n claims = do
         $ throwM $ ValidationException "Nonce mismatch"
 
 -- | Validates the access token claims.
--- 
+--
 -- This function takes a 'Text' value representing the expected issuer
 -- and a 'TokenClaims' value containing the claims to be validated.
 -- It performs the necessary checks to ensure that the claims are valid
